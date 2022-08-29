87af1ee5f04794e05d7d856bc34c9318a045a504
agostinopique <ago.pique@yahoo.it>
2022-08-29 10:10:30 +0200

    Modificato pulsante e label del form create e edit;

--- resources/views/user/dwellings/create.blade.php
+++ resources/views/user/dwellings/create.blade.php
@@ -5,16 +5,16 @@
         <form action="{{ route('user.dwellings.store') }}" method="POST">
             @csrf
 
-            <label for="name">Nome della struttura</label>
             <div class="mb-3">
+                <label for="name">Nome della struttura</label>
                 <input class="form-control" type="text" name="name"
                 @if ($errors->any())
                     value="{{ old('name') }}"
                 @endif>
             </div>
 
-            <label for="category">Tipo di struttura</label>
             <div class="mb-3">
+                <label for="category">Tipo di struttura</label>
                 <select class="form-control" name="category">
                     @foreach ($categories as $category)
 
@@ -28,72 +28,72 @@
                 </select>
             </div>
 
-            <label for="rooms">Numero di stanze</label>
             <div class="mb-3">
+                <label for="rooms">Numero di stanze</label>
                 <input class="form-control" type="number" name="rooms"
                 @if ($errors->any())
                     value="{{ old('rooms') }}"
                 @endif>
             </div>
 
-            <label for="beds">Numero di letti</label>
             <div class="mb-3">
+                <label for="beds">Numero di letti</label>
                 <input class="form-control" type="number" name="beds"
                 @if ($errors->any())
                     value="{{ old('beds') }}"
                 @endif>
             </div>
 
-            <label for="bathrooms">Numero di bagni</label>
             <div class="mb-3">
+                <label for="bathrooms">Numero di bagni</label>
                 <input class="form-control" type="number" name="bathrooms"
                 @if ($errors->any())
                     value="{{ old('bathrooms') }}"
                 @endif>
             </div>
 
-            <label for="dimentions">Metri quadri della struttura</label>
             <div class="mb-3">
+                <label for="dimentions">Metri quadri della struttura</label>
                 <input class="form-control" type="number" name="dimentions"
                 @if ($errors->any())
                     value="{{ old('dimentions') }}"
                 @endif>
             </div>
 
-            <label for="address">Inserisci la via, con civico se presente</label>
             <div class="mb-3">
+                <label for="address">Inserisci la via, con civico se presente</label>
                 <input class="form-control" type="text" name="address"
                 @if ($errors->any())
                     value="{{ old('address') }}"
                 @endif>
             </div>
 
-            <label for="city">Città in cui si trova</label>
             <div class="mb-3">
+                <label for="city">Città in cui si trova</label>
                 <input class="form-control" type="text" name="city"
                 @if ($errors->any())
                     value="{{ old('city') }}"
                 @endif>
             </div>
 
-            <label for="description">Descrizione dell'appartamento</label>
             <div class="mb-3">
+                <label for="description">Descrizione dell'appartamento</label>
                 <input class="form-control" type="text" name="description"
                 @if ($errors->any())
                     value="{{ old('description') }}"
                 @endif>
             </div>
 
-            <label for="image">Carica un'immagine della struttura</label>
             <div class="mb-3">
+                <label for="image">Carica un'immagine della struttura</label>
                 <input class="form-control" type="text" name="image"
                 @if ($errors->any())
                     value="{{ old('image') }}"
                 @endif>
             </div>
 
-            <label for="price">Prezzo per notte</label>
             <div class="mb-3">
+                <label for="price">Prezzo per notte</label>
                 <input class="form-control" type="number" name="price"
                 @if ($errors->any())
                     value="{{ old('price') }}"
@@ -101,7 +101,7 @@
             </div>
 
             <div class="mb-3">
-                <input class="form-control" type="submit" value="Aggiungi">
+                <input class="btn btn-primary" type="submit" value="Aggiungi">
             </div>
 
         </form>
--- resources/views/user/dwellings/edit.blade.php
+++ resources/views/user/dwellings/edit.blade.php
@@ -6,14 +6,14 @@
         @csrf
         @method('PUT')
 
-            <label for="name">Nome della struttura</label>
-            <div class="mb-3">
+        <div class="mb-3">
+                <label for="name">Nome della struttura</label>
                 <input class="form-control" type="text" name="name"
                 value="{{ !$errors->any() ? $dwelling->name : old('name') }}">
             </div>
 
-            <label for="category">Tipo di struttura</label>
             <div class="mb-3">
+                <label for="category">Tipo di struttura</label>
                 <select class="form-control" name="category">
                     @foreach ($categories as $category)
 
@@ -28,62 +28,62 @@
                 </select>
             </div>
 
-            <label for="rooms">Numero di stanze</label>
             <div class="mb-3">
+                <label for="rooms">Numero di stanze</label>
                 <input class="form-control" type="number" name="rooms"
                 value="{{ !$errors->any() ? $dwelling->rooms : old('rooms') }}">
             </div>
 
-            <label for="beds">Numero di letti</label>
             <div class="mb-3">
+                <label for="beds">Numero di letti</label>
                 <input class="form-control" type="number" name="beds"
                 value="{{ !$errors->any() ? $dwelling->beds : old('beds') }}">
             </div>
 
-            <label for="bathrooms">Numero di bagni</label>
             <div class="mb-3">
+                <label for="bathrooms">Numero di bagni</label>
                 <input class="form-control" type="number" name="bathrooms"
                 value="{{ !$errors->any() ? $dwelling->bathrooms : old('bathrooms') }}">
             </div>
 
-            <label for="dimentions">Metri quadri della struttura</label>
             <div class="mb-3">
+                <label for="dimentions">Metri quadri della struttura</label>
                 <input class="form-control" type="number" name="dimentions"
                 value="{{ !$errors->any() ? $dwelling->dimentions : old('dimentions') }}">
             </div>
 
-            <label for="address">Inserisci la via, con civico se presente</label>
             <div class="mb-3">
+                <label for="address">Inserisci la via, con civico se presente</label>
                 <input class="form-control" type="text" name="address"
                 value="{{ !$errors->any() ? $dwelling->address : old('address') }}">
             </div>
 
-            <label for="city">Città in cui si trova</label>
             <div class="mb-3">
+                <label for="city">Città in cui si trova</label>
                 <input class="form-control" type="text" name="city"
                 value="{{ !$errors->any() ? $dwelling->city : old('city') }}">
             </div>
 
-            <label for="description">Descrizione dell'appartamento</label>
             <div class="mb-3">
+                <label for="description">Descrizione dell'appartamento</label>
                 <input class="form-control" type="text" name="description"
                 value="{{ !$errors->any() ? $dwelling->description : old('description') }}">
             </div>
 
-            <label for="image">Carica un'immagine della struttura</label>
             <div class="mb-3">
+                <label for="image">Carica un'immagine della struttura</label>
                 <input class="form-control" type="text" name="image"
                 value="{{ !$errors->any() ? $dwelling->image : old('image') }}">
             </div>
 
-            <label for="price">Prezzo per notte</label>
             <div class="mb-3">
+                <label for="price">Prezzo per notte</label>
                 <input class="form-control" type="number" name="price"
                 value="{{ !$errors->any() ? $dwelling->price : old('price') }}">
             </div>
 
             <div class="mb-3">
-                <input class="form-control" type="submit" value="Aggiorna modifiche">
+                <input class="btn btn-primary" type="submit" value="Aggiorna modifiche">
             </div>
 
     </form>
