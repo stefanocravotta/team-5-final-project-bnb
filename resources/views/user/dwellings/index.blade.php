1923a5ba0ab6e1d57fa8256592bd208e50a483ff
MikeyBadi <mihaibadia.1999@gmial.com>
2022-08-29 18:08:41 +0200

    update - delete button with confirmation popup

--- resources/views/user/dwellings/index.blade.php
+++ resources/views/user/dwellings/index.blade.php
@@ -29,7 +29,29 @@
             <form action="{{ route('user.dwellings.destroy', $dwelling) }}" method="POST">
             @csrf
             @method('DELETE')
-                <input type="submit" value="Delete">
+            <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
+                Elimina
+            </button>
+
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">
+                      ...
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                      <button type="submit" class="btn btn-primary">Delete</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </form>
         @endforeach
 
@@ -52,7 +74,31 @@
             <form action="{{ route('user.dwellings.destroy', $dwelling) }}" method="POST">
             @csrf
             @method('DELETE')
-                <input type="submit" value="Delete">
+                <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
+                    Elimina
+                </button>
+
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+                        <div class="modal-body">
+                          ...
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                          <button type="submit" class="btn btn-primary">Delete</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+
             </form>
         @endforeach
 
@@ -61,5 +107,6 @@
 
 
 
-
 @endsection
+
+
--- resources/views/user/dwellings/show.blade.php
+++ resources/views/user/dwellings/show.blade.php
@@ -12,7 +12,7 @@
         <div class="card">
             @if($dwelling->image)
 
-                <img src="{{ asset('images/'.$dwelling->image) }}" class="card-img-top" alt="{{ $dwelling }}">
+                <img src="{{ asset('images/'.$dwelling->image) }}" class="card-img-top" alt="{{ $dwelling->name }}">
 
             @else
 
@@ -40,7 +40,29 @@
                 <form class="d-inline" action="{{ route('user.dwellings.destroy', $dwelling) }}" method="POST" onsubmit="return confirm('Sei sicuro di voler eliminare l\'appartamento {{ $dwelling->name }}?')">
                     @csrf
                     @method('DELETE')
-                        <input class="btn btn-danger" type="submit" value="Delete">
+                    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
+                        Elimina
+                    </button>
+
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                          <div class="modal-content">
+                            <div class="modal-header">
+                              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                              </button>
+                            </div>
+                            <div class="modal-body">
+                              ...
+                            </div>
+                            <div class="modal-footer">
+                              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                              <button type="submit" class="btn btn-danger">Delete</button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                 </form>
             </div>
         </div>
