f55f246ec231d65222f77f1f32ad4fb59d4b7a1f
agostinopique <ago.pique@yahoo.it>
2022-08-31 12:29:04 +0200

    add - creata draft homepage;

---
+++ resources/js/components/partials/HeaderComp.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="header">
+        <div class="container">
+            <h1>  Sono un header</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComp'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
