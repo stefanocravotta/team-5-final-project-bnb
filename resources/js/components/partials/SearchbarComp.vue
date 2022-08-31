bfc22fc0a443f7a1574b9678f94f8d1159b9b9f1
agostinopique <ago.pique@yahoo.it>
2022-08-31 18:06:23 +0200

    edit - aggiunto stile ad input e pulsante di search;

--- resources/js/components/partials/SearchbarComp.vue
+++ resources/js/components/partials/SearchbarComp.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <input type="text" class="searchbar">
+        <a href="#" class="search-button">Cerca</a>
     </div>
 </template>
 
@@ -14,5 +15,16 @@ export default {
 .searchbar{
     height: 45px;
     width: 45%;
+}
+.search-button{
+    padding: 13px;
+    background-color: rgb(71, 71, 233);
+    color: white;
+    border-radius: 4px;
+    text-decoration: none;
+    & :hover {
+        text-decoration: none;
+        box-shadow: 5px -10px rgb(71, 71, 233) inset;
+    }
 }
 </style>
