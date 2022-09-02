312aac01284a99ffb7d23a73b657ae942c14c010
MikeyBadi <mihaibadia.1999@gmial.com>
2022-09-02 19:15:41 +0200

    edit - show apartment continued visual

--- resources/js/components/pages/showApartment.vue
+++ resources/js/components/pages/showApartment.vue
@@ -1,43 +1,148 @@
 <template>
-  <div>
-    <img :src="'/images/8824717.jpg'" alt="">
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos necessitatibus cum quam a, doloribus culpa. Distinctio quibusdam rem aliquam nobis obcaecati ea exercitationem quasi neque libero, deleniti at magni adipisci laborum. Optio hic fugiat saepe repudiandae nemo dicta ut sequi quisquam neque qui et expedita, eos provident ad quas mollitia eum consequatur reiciendis, explicabo in sint earum? Tempore et quae enim est! Nostrum ullam quidem alias temporibus, voluptate incidunt recusandae quos fuga perspiciatis, ipsam non quod asperiores repudiandae, nihil iure hic soluta. Vero veritatis, vel alias commodi unde, voluptates architecto ad facilis enim perferendis est deserunt sunt dolore ratione, quisquam molestias illo. Necessitatibus mollitia sunt voluptatem ducimus asperiores culpa expedita aspernatur quaerat illum nostrum! Nulla id adipisci sit dolore repellendus, eius quisquam voluptates, placeat error debitis perspiciatis explicabo sunt optio dignissimos consectetur maiores iusto alias repellat impedit quam fugiat laudantium! Sapiente eius aperiam quis eligendi eum vitae consequatur voluptate nobis pariatur soluta unde debitis autem eos iste hic beatae, asperiores veritatis distinctio? Fuga, deleniti? Magnam laborum est facere, vero saepe quisquam placeat aliquam quis, nulla et consequatur maxime iure fugit exercitationem cumque temporibus consequuntur unde voluptatem, repellendus officia debitis voluptas! Unde, eligendi delectus placeat quod optio error quisquam eum iste.
-    </p>
-    <h1>nome casa</h1>
+    <div class="">
+        <div class="container-fluid">
+            <div class="d-flex mb-imgSection">
+                <img
+                class="mb-principalImg"
+                :src="'/images/8824717.jpg'" alt="">
+                <div class="mb-containerSide">
+                    <img
+                    class="mb-sideImg"
+                    :src="'/images/8824717.jpg'" alt="">
+                    <img
+                    class="mb-sideImg"
+                    :src="'/images/8824717.jpg'" alt="">
+                    <!-- <div class="h-3">
+                        <button>Visualizza tutte le foto</button>
+                    </div> -->
+                    <div class="button button-2">Mostra più immagini</div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid mb-proprietyDesc">
+            <h1>Nome casa abbastanza lungo tipo tanto così</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos necessitatibus cum quam a, doloribus culpa. Distinctio quibusdam rem aliquam nobis obcaecati ea exercitationem quasi neque libero, deleniti at magni adipisci laborum. Optio hic fugiat saepe repudiandae nemo dicta ut sequi quisquam neque qui et expedita, eos provident ad quas mollitia eum consequatur reiciendis, explicabo in sint earum? Tempore et quae enim est! Nostrum ullam quidem alias temporibus, voluptate incidunt recusandae quos fuga perspiciatis, ipsam non quod asperiores repudiandae, nihil iure hic soluta. Vero veritatis, vel alias commodi unde, voluptates architecto ad facilis enim perferendis est deserunt sunt dolore ratione, quisquam molestias illo. Necessitatibus mollitia sunt voluptatem ducimus asperiores culpa expedita aspernatur quaerat illum nostrum! Nulla id adipisci sit dolore repellendus, eius quisquam voluptates, placeat error debitis perspiciatis explicabo sunt optio dignissimos consectetur maiores iusto alias repellat impedit quam fugiat laudantium! Sapiente eius aperiam quis eligendi eum vitae consequatur voluptate nobis pariatur soluta unde debitis autem eos iste hic beatae, asperiores veritatis distinctio? Fuga, deleniti? Magnam laborum est facere, vero saepe quisquam placeat aliquam quis, nulla et consequatur maxime iure fugit exercitationem cumque temporibus consequuntur unde voluptatem, repellendus officia debitis voluptas! Unde, eligendi delectus placeat quod optio error quisquam eum iste.
+            </p>
+        </div>
 
-    <div>
-        <div>
-            <p>Categoria:</p>
-            <h3>Nome categoria</h3>
+
+        <div class="containter-fluid">
+            <div class="row row-col row-cols-lg-5 d-flex mb-containerInfo">
+                <div class="mb-infoHome d-flex">
+                    <h2>Categoria:</h2>
+                    <h3>Nome categoria</h3>
+                </div>
+                <div class=" mb-infoHome d-flex">
+                    <h2>Numero di stanze:</h2>
+                    <h3>stanze</h3>
+                </div>
+                <div class=" mb-infoHome d-flex">
+                    <h2>Numero letti:</h2>
+                    <h3>letti</h3>
+                </div>
+                <div class=" mb-infoHome d-flex">
+                    <h2>Numero bagni:</h2>
+                    <h3>bagni</h3>
+                </div>
+                <div class=" mb-infoHome d-flex">
+                    <h2>Dimensioni della proprietà:</h2>
+                    <h3>dimensioni</h3>
+                </div>
+            </div>
+                <div class="mb-positionInfo">
+                    <div class="container-fluid">
+                        <h2>Posizione</h2>
+                        <h3>via caciocavallonico, ovuvueweoniquri OSAS</h3>
+                    </div>
+                    <div class="mb-mappa">
+                        <p>Qui ci sarà la mappa giganterrima</p>
+                    </div>
+                </div>
         </div>
-        <div>
-            <p>Numero di stanze:</p>
-            <h3>stanze</h3>
-        </div>
-        <div>
-            <p>Numero letti:</p>
-            <h3>letti</h3>
-        </div>
-        <div>
-            <p>Numero bagni:</p>
-            <h3>bagni</h3>
-        </div>
-        <div>
-            <p>Dimensioni della proprietà:</p>
-            <h3>dimensioni</h3>
-        </div>
-        <div>
-            <p>Posizione</p>
-            <h3>Via e Paese</h3>
+
+        <!-- PERKS -->
+        <div class="mb-services d-flex justify-content-between">
+            <h3>Lista servizi</h3>
+            <div class="mb-perks">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-3 mb-rowPerks">
+                            <i>FA</i>
+                            <p>Servizi</p>
+                        </div>
+                <!-- ||| delete for the cyle now just for test ||| -->
+                        <div class="col-3 mb-rowPerks">
+                            <i>FA</i>
+                            <p>Servizi</p>
+                        </div>
+                        <div class="col-3 mb-rowPerks">
+                            <i>FA</i>
+                            <p>Servizi</p>
+                        </div>
+                        <div class="col-3 mb-rowPerks">
+                            <i>FA</i>
+                            <p>Servizi</p>
+                        </div>
+                        <div class="col-3 mb-rowPerks">
+                            <i>FA</i>
+                            <p>Servizi</p>
+                        </div>
+                        <div class="col-3 mb-rowPerks">
+                            <i>FA</i>
+                            <p>Servizi</p>
+                        </div>
+                        <div class="col-3 mb-rowPerks">
+                            <i>FA</i>
+                            <p>Servizi</p>
+                        </div>
+                        <div class="col-3 mb-rowPerks">
+                            <i>FA</i>
+                            <p>Servizi</p>
+                        </div>
+                        <div class="col-3 mb-rowPerks">
+                            <i>FA</i>
+                            <p>Servizi</p>
+                        </div>
+                        <div class="col-3 mb-rowPerks">
+                            <i>FA</i>
+                            <p>Servizi</p>
+                        </div>
+                        <div class="col-3 mb-rowPerks">
+                            <i>FA</i>
+                            <p>Servizi</p>
+                        </div>
+                        <div class="col-3 mb-rowPerks">
+                            <i>FA</i>
+                            <p>Servizi</p>
+                        </div>
+                        <div class="col-3 mb-rowPerks">
+                            <i>FA</i>
+                            <p>Servizi</p>
+                        </div>
+                        <div class="col-3 mb-rowPerks">
+                            <i>FA</i>
+                            <p>Servizi</p>
+                        </div>
+                        <div class="col-3 mb-rowPerks">
+                            <i>FA</i>
+                            <p>Servizi</p>
+                        </div>
+                        <div class="col-3 mb-rowPerks">
+                            <i>FA</i>
+                            <p>Servizi</p>
+                        </div>
+                        <div class="col-3 mb-rowPerks">
+                            <i>FA</i>
+                            <p>Servizi</p>
+                        </div>
+                <!-- ||| delete for the cyle now just for test ||| -->
+                    </div>
+                </div>
+
+            </div>
         </div>
     </div>
-        <div>
-            <h3>Lista servizi</h3>
-            <p>Servizi</p>
-        </div>
-
-  </div>
 </template>
 
 <script>
@@ -47,6 +152,169 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+// sezione immagini
+.mb-imgSection{
+        img:hover{
+            filter: brightness(80%);
+        }
+        .mb-principalImg{
+            width: 68.2%;
+        }
+        .mb-containerSide{
+            width: 31.8%;
+            margin-left: 2.7%;
+
+            .mb-sideImg{
+                width: 100%;
+                margin-bottom: 10%;
+            }
+
+
+            // button show more
+            .button {
+                width: 100%;
+                padding-top: 30px;
+                padding-bottom: 30px;
+                text-align: center;
+                color: #000;
+                text-transform: uppercase;
+                font-weight: 600;
+                cursor: pointer;
+                display: inline-block;
+            }
+            .button-2 {
+                color: #fff;
+                border: 3px solid #000000;
+                background-image: -webkit-linear-gradient(30deg, #000000 50%, transparent 50%);
+                background-image: linear-gradient(30deg, #000000 50%, transparent 50%);
+                background-size: 300%;
+                background-repeat: no-repeat;
+                background-position: 0%;
+                -webkit-transition: background 300ms ease-in-out;
+                transition: background 300ms ease-in-out;
+            }
+            .button-2:hover {
+                background-position: 100%;
+                color: #000000;
+            }
+        }
+    }
+
+// descrizione con titolo dell'abitazione + descrizione di 200parole
+    .mb-proprietyDesc{
+        h1{
+            font-size: 90px;
+        }
+        p{
+            font-weight: bold;
+            font-size: 16px;
+            width: 70%;
+            padding-bottom: 100px;
+        }
+        @media screen and (max-width: 465px){
+            h1{
+                font-size: 300%;
+            }
+            p{
+                font-size: 70%;
+                width: 100%;
+
+            }
+        }
+    }
+
+// container delle info
+    .mb-containerInfo{
+        margin: 0;
+        .mb-infoHome{
+            border: 1px solid rgb(255, 103, 43);
+            border-bottom: 2px solid rgb(255, 103, 43);
+            border-top: 2px solid rgb(255, 103, 43);
+            // box-sizing: border-box;
+            flex-grow: 1;
+            width: 100%;
+            height: 150px;
+            overflow: scroll;
+            flex-direction: column;
+
+            h2{
+                color: rgb(124, 124, 124);
+                font-size: 16px;
+                padding: 10px;
+                align-self: start;
+            }
+            h3{
+                word-wrap: break-word;
+                text-align: center;
+                margin: 0;
+                align-self: center;
+                justify-self: center;
+            }
+        }
+        @media screen and (max-width: 992px) {
+            .mb-infoHome{
+                border-bottom: 1px solid rgb(255, 103, 43);
+                border-top: 1px solid rgb(255, 103, 43);
+            }
+            .mb-infoHome:first-of-type{
+                border-top:  2px solid rgb(255, 103, 43);
+                border-left:  2px solid rgb(255, 103, 43);
+            }
+            .mb-infoHome:last-of-type{
+                border-bottom:  2px solid rgb(255, 103, 43);
+                border-right:  2px solid rgb(255, 103, 43);
+            }
+        }
+    }
+
+    // container della mappa
+    .mb-positionInfo{
+        width: 100%;
+        border-bottom:  2px solid rgb(255, 103, 43);
+        padding-top: 30px;
+        .mb-mappa{
+            background-color: rgb(173, 171, 145);
+            width: 100%;
+            height: 85vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            margin: 30px 0px 20px;
+        }
+    }
+    .mb-services{
+        margin-top: 35px;
+        h3{
+            width: 25%;
+            height: fit-content;
+            margin-top: 10vh;
+            color: rgb(124, 124, 124);
+            padding: 12.5px 5px;
+            border-top: 1px solid rgb(124, 124, 124);
+            border-bottom: 1px solid rgb(124, 124, 124);
+            text-align: center;
+        }
+        .mb-perks{
+            margin: 0 auto;
+            width: 45%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+            // background-color: khaki;
+            .mb-rowPerks{
+                padding-top: 12.5px;
+                // border-bottom: 2px solid rgb(255, 103, 43);
+                width: 80%;
+                text-align: center;
+                p{
+                    display: inline;
+                }
+            }
+        }
+    }
+
+
 
 </style>
