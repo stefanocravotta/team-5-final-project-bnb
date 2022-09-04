bbd71b710cd068a95745f0fc1df10269fc664792
agostinopique <ago.pique@yahoo.it>
2022-09-04 14:08:50 +0200

    add - Aggiunte funzioni incomplete per la ricerca di appartamenti in base ai perk;

--- resources/js/components/pages/SearchresultsComp.vue
+++ resources/js/components/pages/SearchresultsComp.vue
@@ -1,11 +1,30 @@
 <template>
     <div class="container">
         <h1>Risultati della ricerca</h1>
-        <p>{{$route.params.city}}</p>
 
-        <p v-for="apartment in apartments" :key="apartment.id">{{ apartment.name}}</p>
+        <div v-if="!isFiltered">
+
+            <p v-for="apartment in apartments" :key="apartment.id">{{ apartment.name}}</p>
+
+        </div>
+
+        <div v-if="isFiltered">
+
+            <p v-for="apartment in filtered_apartments" :key="apartment.id">{{ apartment.name}}</p>
+
+        </div>
+
+        <form id="perk-form">
+            <div v-for="perk in perks" :key="perk.id" class="d-inline mx-2">
+                <label :for="perk.name">{{ perk.name }}</label>
+                <input type="checkbox" name="perk-box" :id="perk.name" :value="perk.id">
+            </div>
+
+            <button type="submit" @click.prevent="checkPerks()">Go!</button>
+        </form>
 
         <button class="btn btn-primary mx-2" v-for="category in categories" :key="category.id" @click="searchByCategory(category.id)">{{category.name}}</button>
+
     </div>
 </template>
 
@@ -18,7 +37,9 @@ export default {
             apiUrl: '/api/dwellings',
             apartments: null,
             perks: null,
-            categories: null
+            categories: null,
+            filtered_apartments: [],
+            isFiltered: false
         }
     },
 
@@ -30,17 +51,27 @@ export default {
                 this.apartments = r.data.dwellings;
                 this.perks = r.data.perks;
                 this.categories = r.data.categories;
-                console.log(r.data);
+                // console.log(r.data);
             })
         },
 
         searchByCategory(categoryId){
+            let pivot_array = [];
+            this.apartments.forEach(el =>{
+
+                if(el.category == categoryId){
+                    pivot_array.push(el)
+                }
 
 
-
+            })
+            this.filtered_apartments = pivot_array;
+            this.isFiltered = true;
 
             // console.log('ciao', categoryId);
             // console.log('apartment', this.apartmentSearch);
+
+            // ALTERNATIVA CON CHIAMATA AXIOS
 
             // axios.get(this.apiUrl + '/search-category/' + categoryId + '/' + this.apartmentSearch)
             // .then(r =>{
@@ -48,7 +79,33 @@ export default {
             //     console.log(r.data.apartment);
 
             // })
+        },
+
+        checkPerks(){
+            let checked = [];
+
+            // Prendo i valori dei perk che ho cercato e li salvo il un array
+            let markedCheckbox = document.getElementsByName('perk-box');
+            for (let checkbox of markedCheckbox) {
+                if (checkbox.checked){ checked.push(checkbox.value) }
+            }
+            // console.log(checked);
+
+            let perked_apartments = [];
+            let apartment_perk_arr = [];
+            this.apartments.forEach(apartment => {
+                // console.log(apartment.perks);
+                apartment.perks.forEach(perk => {
+
+                    // Restituisce un array di array con parametri chiave - valore;
+                    for (const [key, value] of Object.entries(perk)) {
+                        console.log(`${key}: ${value}`);
+                    }
+                });
+            });
         }
+
+
     },
     mounted(){
         this.searchDwelling();
