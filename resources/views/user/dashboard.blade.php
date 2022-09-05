eb0a873f5c9262109493add6a0ead9dd93754270
DavideManciucca <davidemanciucca@gmail.com>
2022-09-05 11:13:30 +0200

    aggiunta bozza dashboard

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -2,37 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Il tuo profilo</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                </div>
-                <div class="card-body">
-                    <a href="{{ route('user.messages') }}">Accedi a i tuoi messaggi</a>
-                </div>
-                <div class="card-body">
-                    <a class="navbar-brand" href="{{ route('user.dwellings.index') }}">
-                        Appartamenti
-                    </a>
-                </div>
-                <div class="card-body">
-
-                    <a class="navbar-brand" href="{{ route('user.dwellings.create') }}">
-                        Aggiungi appartamenti
-                    </a>
-                </div>
-            </div>
-        </div>
-<<<<<<< Updated upstream
-=======
-    </div> --}}
     <div class="wrapper">
 
 {{-- box sinistra --}}
@@ -79,7 +49,6 @@
             @endfor
           </div>
         </div>
->>>>>>> Stashed changes
     </div>
 </div>
 @endsection
