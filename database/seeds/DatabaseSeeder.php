2e395893155e04e4a02ec8e90314810bfbdf554c
agostinopique <ago.pique@yahoo.it>
2022-09-07 16:06:12 +0200

    edit - Aggiunte icone nel seeder dei perk;

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(CategoriesTableSeeder::class);
-        // $this->call(PerksTableSeeder::class);
-        // $this->call(SponsorisationsTableSeeder::class);
-        $this->call(MessagesTableSeeder::class );
+        $this->call(CategoriesTableSeeder::class);
+        $this->call(PerksTableSeeder::class);
+        $this->call(SponsorisationsTableSeeder::class);
+        // $this->call(MessagesTableSeeder::class );
 
     }
 }
